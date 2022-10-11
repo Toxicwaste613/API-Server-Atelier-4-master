61930ca18318acc8bf3745532358626815599e8e
Toxicwaste613 <lucas.mercure@hotmail.com>
2022-10-11 14:19:23 -0400

    first commit

---
+++ models/nouvelle.js
@@ -0,0 +1,18 @@
+const Model = require('./model');
+module.exports =
+    class Nouvelle extends Model{
+        constructor(category, title, text, imageUrl, date) {
+            super();
+            this.Category = category !== undefined ? category : "";
+            this.Title = title !== undefined ? title : "";
+            this.Text = text !== undefined ? text : "";
+            this.ImageUrl = imageUrl !== undefined ? imageUrl : "";
+            this.Date = date !== undefined ? date : "";
+
+            this.setKey("")
+            this.addValidator('Category','string');
+            this.addValidator('Title', 'string');
+            this.addValidator('ImageUrl', 'url');
+            this.addValidator('Date','date');
+        }
+    }
